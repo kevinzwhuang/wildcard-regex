ffa0150a1c80c4772e83f8cc6b1ef264a49914dd
Kevin Huang <kevin.ziwen.huang@gmail.com>
2016-10-22 22:35:34 -0700

    Add regex tranformation feature for arrays of strings

--- wildcard-regex.js
+++ wildcard-regex.js
@@ -1,11 +1,11 @@
 "use strict";
 
-function transformWildcardToPattern(wildcard) {
-  var length = wildcard.length;
+function transformWildcardToPattern(wildcardString) {
+  var length = wildcardString.length;
   var character;
   var pattern = '';
   for (var index = 0; index < length; index++) {
-    character = wildcard.charAt(index);
+    character = wildcardString.charAt(index);
     if (character === '*') {
       pattern = pattern + '.*'
     } else {
@@ -15,8 +15,30 @@ function transformWildcardToPattern(wildcard) {
   return pattern;
 };
 
-function wildcardRegex(string) {
-  var pattern = transformWildcardToPattern(string)
+function transformArrayToPattern(arrayOfWildcardStrings) {
+  var length = arrayOfWildcardStrings.length;
+  var wildcardString;
+  var nextSubPattern;
+  var pattern = '';
+  for (var index = 0; index < length; index++) {
+    wildcardString = arrayOfWildcardStrings[index];
+    nextSubPattern = transformWildcardToPattern(wildcardString);
+    if (index === 0) {
+      pattern = pattern + nextSubPattern;
+    } else {
+      pattern = pattern + '|' + nextSubPattern;
+    }
+  }
+  return pattern;
+};
+
+function wildcardRegex(stringOrArray) {
+  var pattern;
+  if (stringOrArray.constructor === String) {
+    pattern = transformWildcardToPattern(string)
+  } else if (stringOrArray.constructor === Array) {
+    pattern = transformArrayToPattern(stringOrArray)
+  }
   var regex = new RegExp(pattern);
   return regex;
 };
