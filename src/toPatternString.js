4538dbeff756201bf1ad0cc6338153de292feaaa
Kevin Huang <kevin.ziwen.huang@gmail.com>
2016-12-03 16:28:42 -0800

    Move pattern building logic to toPatternString.js

---
+++ src/toPatternString.js
@@ -0,0 +1,48 @@
+function stringToPattern(inputString) {
+  var length = inputString.length;
+  var character;
+  var pattern = '^';
+  for (var index = 0; index < length; index++) {
+    character = inputString.charAt(index);
+    if (character === '*') {
+      pattern = pattern + '.*'
+    } else {
+      pattern = pattern + character;
+    }
+  }
+  pattern = pattern + '$';
+  return pattern;
+};
+
+function arrayToPattern(inputArray) {
+  var length = inputArray.length;
+  var wildcardString;
+  var nextSubPattern;
+  var pattern = '';
+  for (var index = 0; index < length; index++) {
+    wildcardString = inputArray[index];
+    nextSubPattern = stringToPattern(wildcardString);
+    if (index === 0) {
+      pattern = pattern + nextSubPattern;
+    } else {
+      pattern = pattern + '|' + nextSubPattern;
+    }
+  }
+  return pattern;
+};
+
+function toPatternString(stringOrArray) {
+  var pattern;
+  if (stringOrArray.constructor === String) {
+    pattern = transformWildcardToPattern(stringOrArray);
+  } else if (stringOrArray.constructor === Array) {
+    pattern = transformArrayToPattern(stringOrArray);
+  } else {
+    throw TypeError('WildcardRegex only accepts a string or array as an argument');
+  }
+  return pattern;
+  var regex = new RegExp(pattern);
+  return regex;
+};
+
+export default toPatternString;
